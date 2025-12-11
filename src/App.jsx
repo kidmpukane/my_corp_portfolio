0ad278a995f8ea3d03b7b6cbc3635697eb9ad5f5
kidmpukane <lwandisozwane91@gmail.com>
2025-12-11 13:52:46 +0200

    nav styles initialsed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AboutMe from "./pages/AboutMe";
 import InfiniteScrollPage from "./pages/InfiniteScroll";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
+      <h1 className="app-header">LWANDISO ZWANE</h1>
+      <h2 className="app-subheader">MACHINE LEARNING ENGINEER</h2>
       <InfiniteScrollPage />
     </div>
   );
